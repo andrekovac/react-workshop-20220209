1f33235fe8a68f32274f32eccefe68087d7bfc7f
André Kovac <me@andrekovac.com>
2022-02-11 09:46:03 +0100

    useState hook changed

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,32 @@
 import { useState } from "react";
 import "./App.css";
 
-// let count = 0;
-
 function App() {
-  const [count, setCount] = useState(0);
+  const [c, setC] = useState<number>(0);
 
   const handleClick = () => {
-    // count++;
-    setCount(count + 1);
-    return console.log("hallo", count);
+    setC(c + 1);
   };
 
   return (
     <>
-      <div onClick={handleClick}>Hallo das ist mein Count: {count}</div>
+      <div onClick={handleClick}>
+        Hallo das ist mein Count: {count} das ist ein weiterer Text
+      </div>
       <div>Hallo das bin ich wieder</div>
     </>
   );
 }
 
 export default App;
+
+// const handleClick = () => {
+//   countInside++;
+//   setCount1(count + 1);
+//   setCount2(count + 2);
+
+//   setCounts([counts[0] + 1, counts[1] + 2]);
+
+//   console.log("setState:", count1);
+//   console.log("countInside", countInside);
+// };
