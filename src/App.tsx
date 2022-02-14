e156c70631c78223fb3992a7d90b06f04b5056c5
André Kovac <me@andrekovac.com>
2022-02-14 10:53:39 +0100

    useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,27 @@
+import { useState } from "react";
 import "./App.css";
 import Counter from "./components/Counter";
 import Name from "./components/Name";
 
 const App = () => {
+  const [showCounter, setShowCounter] = useState(true);
+
   const names = ["Andre", "Hans", "Janine", "Michelle"];
 
   // <Name>Andre</Name>
 
   return (
     <div className="App">
-      <Counter />
+      <button
+        onClick={() => {
+          setShowCounter(!showCounter);
+        }}
+      >
+        Toggle Counter View
+      </button>
+
+      {showCounter ? <Counter /> : null}
+      {/* {<Counter />} */}
       {names.map((name) => {
         return <Name key={name}>{name}</Name>;
       })}
--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,15 +1,31 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const Counter: React.VFC = () => {
   const [count, setCount] = useState(0);
+  const [value, setValue] = useState<string | number>("foo");
+
+  useEffect(() => {
+    console.log("effect");
+    // subscribed
+
+    // return () => {
+    //   console.log("clean up");
+    //   // unsubscribed
+    // };
+  }, [value]); // {} === {}
 
   const incrementCount = (
     event: React.MouseEvent<HTMLButtonElement, MouseEvent>
   ) => {
-    setCount((prevCount) => {
-      return prevCount + 1;
-    });
+    setCount(count + 1);
+    console.log(count);
+    if (count > 5) {
+      // bar -> bar
+      setValue("bar");
+    }
   };
+
+  console.log("render");
 
   return <button onClick={incrementCount}>{`Count: ${count}`}</button>;
 };
