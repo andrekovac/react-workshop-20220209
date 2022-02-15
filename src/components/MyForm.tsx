751070f324f42bfce3f26ab1c9eeda5b20b31bac
André Kovac <me@andrekovac.com>
2022-02-15 11:46:21 +0100

    Form

--- src/components/MyForm.tsx
+++ src/components/MyForm.tsx
@@ -1,5 +1,27 @@
+import { FormEvent, useState } from "react";
+
 const MyForm: React.FC = () => {
-  return <form></form>;
+  const [name, setName] = useState("");
+
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+    console.log(event);
+    console.log(name);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="name">Name</label>
+      <input
+        id="name"
+        type="email"
+        placeholder="Your name"
+        value={name}
+        onChange={(event) => setName(event.target.value)}
+      />
+      <button type="submit">Name submitten</button>
+    </form>
+  );
 };
 
 export default MyForm;
